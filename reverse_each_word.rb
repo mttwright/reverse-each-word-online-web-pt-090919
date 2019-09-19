1e573b40d6a8646a91f52a7137219d0bb3d90143
virtual-frame-7789 <mttwright@gmail.com>
2019-09-19 22:29:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+#def reverse_each_word(string)
+#  word_array = string.split
+#  reversed_array = []
+#  word_array.each do |word|
+#    reversed_array.push(word.reverse + " ")
+#  end
+#  reversed_sentence = reversed_array.join
+#  reversed_sentence.chop!
+#end
+
+
+def reverse_each_word(string)
+  word_array = string.split
+  reversed_array = word_array.collect do |word|
+    word.reverse
+  end
+  reversed_array.join(" ")
+end
