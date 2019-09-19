5aa92320a81cbee74d9de3a4bd5690ad82bc8f08
virtual-frame-7789 <mttwright@gmail.com>
2019-09-19 22:22:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(string)
-  word_array = string.split
-  reversed_array = []
-  word_array.each do |word|
-    reversed_array.push(word.reverse + " ")
-  end
-  reversed_sentence = reversed_array.join
-  reversed_sentence.chop!
-end
+#def reverse_each_word(string)
+#  word_array = string.split
+#  reversed_array = []
+#  word_array.each do |word|
+#    reversed_array.push(word.reverse + " ")
+#  end
+#  reversed_sentence = reversed_array.join
+#  reversed_sentence.chop!
+#end
 
 
 def reverse_each_word(string)
