3289799c71a4b88b39fa07c222d84ccc3f7542dd
virtual-frame-7789 <mttwright@gmail.com>
2019-09-19 22:14:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   word_array = string.split
   reversed_array = []
   word_array.each do |word|
-    reversed_array.push(word.reverse)
+    reversed_array.push(word.reverse + " ")
   end
   reversed_array.join
 end
